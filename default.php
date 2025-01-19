77df2203727305521433e4d857bb1e610e442dbb
Daniel <brienzadani@gmail.com>
2025-01-19 15:51:14 +0100

    Prima prova di autoaggiornamento

---
+++ default.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Sito Web in Costruzione</title>
+        <link rel="icon" href="logo.png">
+        <meta charset="utf-8">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta content="Default page" name="description">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
+        <body>
+            <style>
+            *{
+                    margin: 0;
+                    padding: 0;
+            }
+            body{
+                    overflow-Y: hidden;
+            }
+            .lottie{
+                width: 100vh; 
+                height: 100vh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            
+           @media only screen and (max-width: 768px){
+                .lottie{
+                    width: 400px;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+            }
+            </style>
+<script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script>
+<center>
+<dotlottie-player class="lottie" src="https://lottie.host/00dd0ad7-6f02-40b9-84c5-dcff5e88a807/w0xgrszBME.lottie" background="transparent" speed="0.5" loop autoplay></dotlottie-player>
+</center>
+        </body>
+</html>
